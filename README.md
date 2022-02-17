e81f83568a6ed79fdc78dc25daf14dcbc0f06a6f
matthewclouthier <mclouthier@wisc.edu>
2022-02-17 16:30:26 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-dwsehz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-dwsehz)
---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>You got games on yo phone?</h1>
+  <a href="https://www.youtube.com/watch?v=2ZIpFytCSVc" target="_blank">BRUH</a>
+`
